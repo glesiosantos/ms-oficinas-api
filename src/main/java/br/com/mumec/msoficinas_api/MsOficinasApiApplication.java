9f2660c09cc91897a863bf0a02916dcc82064b61
glesio.santos <glesioss@gmail.com>
2025-02-07 13:00:30 -0300

    inicializando projeto

---
+++ src/main/java/br/com/mumec/msoficinas_api/MsOficinasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mumec.msoficinas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOficinasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOficinasApiApplication.class, args);
+	}
+
+}
